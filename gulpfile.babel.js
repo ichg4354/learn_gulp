217be626b7e8dbede5d82fe896115f78ab370110
user <60804817+ichg4354@users.noreply.github.com>
2020-09-18 19:51:15 +0900

    finish until creating and installing gulp webserver

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,26 @@
 import gulp from "gulp";
+import gPug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver";
+
+const paths = {
+  pug: {
+    src: "src/*.pug",
+    dest: "build",
+  },
+};
+
+const pug = () =>
+  gulp.src(paths.pug.src).pipe(gPug()).pipe(gulp.dest(paths.pug.dest));
+
+const clean = () => del(["build"]);
+
+const webserver = () => gulp.src("build").pipe(ws({ livereload: true }));
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+const preBuild = gulp.series([webserver]);
+
+export const dev = gulp.series([prepare, assets, preBuild]);
