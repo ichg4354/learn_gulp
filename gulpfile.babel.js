743d866dc1947e063d4918eda85c65a36874f189
user <60804817+ichg4354@users.noreply.github.com>
2020-09-19 19:59:48 +0900

    install babelify and browserify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,11 @@ import gPug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import img from "gulp-image";
+import scss from "gulp-sass";
+import apref from "gulp-autoprefixer";
+import csso from "gulp-csso";
+import browserify from "gulp-bro";
+import babelify from "babelify";
 
 const paths = {
   pug: {
@@ -15,6 +20,16 @@ const paths = {
     src: "src/img/*",
     dest: "build/img",
   },
+  styles: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/styles.scss",
+    dest: "build/css",
+  },
+  js: {
+    watch: "src/js/*",
+    src: "src/js/main.js",
+    dest: "build/js",
+  },
 };
 
 const pug = () =>
@@ -24,18 +39,41 @@ const clean = () => del(["build"]);
 
 const webserver = () => gulp.src("build").pipe(ws({ livereload: true }));
 
-const watch = () => {
-  gulp.watch(paths.pug.watch, pug);
-  gulp.watch(paths.img.watch, image);
-};
-
 const image = () =>
   gulp.src(paths.img.src).pipe(img()).pipe(gulp.dest(paths.img.dest));
 
+const styles = () =>
+  gulp
+    .src(paths.styles.src)
+    .pipe(scss().on("error", scss.logError))
+    .pipe(apref())
+    .pipe(csso())
+    .pipe(gulp.dest(paths.styles.dest));
+
+const js = () =>
+  gulp
+    .src(paths.js.src)
+    .pipe(
+      browserify({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
+      })
+    )
+    .pipe(gulp.dest(paths.js.dest));
+
+const watch = () => {
+  gulp.watch(paths.pug.watch, pug);
+  gulp.watch(paths.img.watch, image);
+  gulp.watch(paths.styles.watch, styles);
+  gulp.watch(paths.js.watch, js);
+};
+
 const prepare = gulp.series([clean, image]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles, js]);
 
-const preBuild = gulp.series([watch]);
+const preBuild = gulp.series([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, preBuild]);
